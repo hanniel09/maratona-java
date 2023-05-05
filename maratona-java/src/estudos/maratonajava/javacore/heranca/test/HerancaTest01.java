8bd70c701c9b4484810896083c1eb04c7ff98dff
hanniel09 <hannielvieira1227@gmail.com>
2023-05-05 18:48:54 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/heranca/test/HerancaTest01.java
@@ -0,0 +1,17 @@
+package estudos.maratonajava.javacore.heranca.test;
+
+import estudos.maratonajava.javacore.heranca.dominio.Endereco;
+import estudos.maratonajava.javacore.heranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("01234-380");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Pedro");
+        pessoa.setCpf("123.456.789-01");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+    }
+}
