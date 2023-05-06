915d314943bea0f761ec09d090ed33f9ff0cf057
hanniel09 <hannielvieira1227@gmail.com>
2023-05-06 16:06:17 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/sobreescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package estudos.maratonajava.javacore.sobreescrita.test;
+
+
+import estudos.maratonajava.javacore.sobreescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr.Stone");
+        System.out.println(anime);
+    }
+}
