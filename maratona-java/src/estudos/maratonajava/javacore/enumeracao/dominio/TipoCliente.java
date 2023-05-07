f36fde513c57dacab18bc749a475868c0f068a78
hanniel09 <hannielvieira1227@gmail.com>
2023-05-07 17:22:51 -0300

    Feature

--- maratona-java/src/estudos/maratonajava/javacore/enumeracao/dominio/TipoCliente.java
+++ maratona-java/src/estudos/maratonajava/javacore/enumeracao/dominio/TipoCliente.java
@@ -1,6 +1,22 @@
 package estudos.maratonajava.javacore.enumeracao.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Juridica");
+
+    public final int VALOR;
+    private String nomeRelatorio;
+
+    TipoCliente(int valor, String nomeRelatorio) {
+        this.VALOR = valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public int getVALOR() {
+        return VALOR;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
 }
--- maratona-java/src/estudos/maratonajava/javacore/enumeracao/test/ClienteTest01.java
+++ maratona-java/src/estudos/maratonajava/javacore/enumeracao/test/ClienteTest01.java
@@ -5,8 +5,8 @@ import estudos.maratonajava.javacore.enumeracao.dominio.TipoCliente;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente01 = new Cliente("Tsubaza", TipoCliente.PESSOA_FISICA);
-        Cliente cliente02 = new Cliente("Tsubaza", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente01 = new Cliente("Tsubaza", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente02 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA,Cliente.TipoPagamento.DEBITO);
         System.out.println(cliente01);
         System.out.println(cliente02);
 
