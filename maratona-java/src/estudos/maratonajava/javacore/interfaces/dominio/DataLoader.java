75f02ea45659a7e66e2e3485be676258e86df6f2
hanniel09 <hannielvieira1227@gmail.com>
2023-05-09 22:06:57 -0300

    Feature

--- maratona-java/src/estudos/maratonajava/javacore/interfaces/dominio/DataLoader.java
+++ maratona-java/src/estudos/maratonajava/javacore/interfaces/dominio/DataLoader.java
@@ -1,9 +1,12 @@
 package estudos.maratonajava.javacore.interfaces.dominio;
 
 public interface DataLoader {
-
+    public static final int MAX_DATA_SIZE = 10;
     public void load();
     public default void checkPermission(){
         System.out.println("Fazendo a checagem de permissões");
+    }
+    public static void retrievMaxDataSize(){
+        System.out.println();
     }
 }
