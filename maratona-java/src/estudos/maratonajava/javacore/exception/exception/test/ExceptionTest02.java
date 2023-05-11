b2945a0c329fccc94c760eb9304be3a205385570
hanniel09 <hannielvieira1227@gmail.com>
2023-05-11 18:21:21 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/exception/exception/test/ExceptionTest02.java
@@ -0,0 +1,24 @@
+package estudos.maratonajava.javacore.exception.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest02 {
+    public static void main(String[] args)throws IOException {
+        criarNovoArquivo();
+
+    }
+    public static void criarNovoArquivo() throws IOException{
+        File file = new File("arquivo\\teste.txt");
+        try{
+            boolean isNewFile = file.createNewFile();
+            System.out.println("Arquivo criado " + isNewFile);
+        }catch (IOException e){
+            e.printStackTrace();
+            throw e;
+        }
+
+
+
+    }
+}
