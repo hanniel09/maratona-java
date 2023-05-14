437dc37f8e09d39d64327fde26d3d0c750d3988d
hanniel09 <hannielvieira1227@gmail.com>
2023-05-14 18:04:38 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/string/test/StringTest01.java
@@ -0,0 +1,10 @@
+package estudos.maratonajava.javacore.string.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        String nome = "Hanniel"; // String constant pool
+        String nome2 = "Hanniel";
+        System.out.println(nome == nome2);
+
+    }
+}
