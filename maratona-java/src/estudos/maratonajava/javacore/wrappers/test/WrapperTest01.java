0e1fc888666f26f66e5cdda6eeca1d388691bb7b
hanniel09 <hannielvieira1227@gmail.com>
2023-05-14 00:32:48 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/wrappers/test/WrapperTest01.java
@@ -0,0 +1,41 @@
+package estudos.maratonajava.javacore.wrappers.test;
+
+public class WrapperTest01 {
+    public static
+    void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'W';
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L; //boxing
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean booleanW = false;
+
+        int i = intW; //unboxing
+        Integer intW2 = Integer.parseInt("1");
+        Boolean verdadeiro = Boolean.parseBoolean("True");
+        System.out.println(verdadeiro);
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toUpperCase('a'));
+        System.out.println(Character.toLowerCase('A'));
+
+
+
+
+    }
+}
