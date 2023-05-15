151391f71875b8352a02334a7f1a91e34340ae72
hanniel09 <hannielvieira1227@gmail.com>
2023-05-15 00:31:08 -0300

    Feature

--- maratona-java/src/estudos/maratonajava/javacore/string/test/StringTest01.java
+++ maratona-java/src/estudos/maratonajava/javacore/string/test/StringTest01.java
@@ -4,7 +4,11 @@ public class StringTest01 {
     public static void main(String[] args) {
         String nome = "Hanniel"; // String constant pool
         String nome2 = "Hanniel";
+        nome = nome.concat(" Vieira");
+        System.out.println(nome);
         System.out.println(nome == nome2);
+        String nome3 = new String("Hanniel");
+        System.out.println(nome2 == nome3.intern());
 
     }
 }
