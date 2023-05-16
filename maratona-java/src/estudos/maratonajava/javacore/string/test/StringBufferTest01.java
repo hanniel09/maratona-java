7a00c8777c4d6b643adbe610f019a6c6cd99820f
hanniel09 <hannielvieira1227@gmail.com>
2023-05-15 23:59:26 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/string/test/StringBufferTest01.java
@@ -0,0 +1,10 @@
+package estudos.maratonajava.javacore.string.test;
+
+public class StringBufferTest01 {
+    public static void main(String[] args) {
+        String nome = "Hanniel Vieira";
+        nome.concat(" Backend Developer ");
+        System.out.println(nome);
+    }
+}
+oi
