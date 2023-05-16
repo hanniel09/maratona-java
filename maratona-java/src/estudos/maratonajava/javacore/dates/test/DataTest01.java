2ef933fdd4c4a6636988484424e1d98b6473ffd9
hanniel09 <hannielvieira1227@gmail.com>
2023-05-16 19:45:24 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/dates/test/DataTest01.java
@@ -0,0 +1,12 @@
+package estudos.maratonajava.javacore.dates.test;
+
+import java.util.Date;
+
+public class DataTest01 {
+    public static void main(String[] args) {
+        Date data = new Date(1684277044716L);
+        data.setTime(data.getTime() + 3_600_000);
+        System.out.println(data);
+
+    }
+}
--- maratona-java/src/estudos/maratonajava/javacore/string/test/StringBufferTest01.java
+++ maratona-java/src/estudos/maratonajava/javacore/string/test/StringBufferTest01.java
@@ -4,9 +4,12 @@ public class StringBufferTest01 {
     public static void main(String[] args) {
         String nome = "Hanniel Vieira";
         nome.concat(" Alves");
+        nome = nome.substring(0, 3);
         System.out.println(nome);
-        StringBuilder sb = new StringBuilder("Hanniel Vieira");
-        sb.append(" Alves");
+        StringBuffer sb = new StringBuffer("Hanniel Vieira");
+        sb.append(" Alves").append(" Silva");
+        //sb.reverse();
+        sb.delete(0,3);
         System.out.println(sb);
     }
 }
