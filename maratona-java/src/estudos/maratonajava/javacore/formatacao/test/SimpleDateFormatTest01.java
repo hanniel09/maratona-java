b82bcd30b444fbae02407a5c289ef5a2a267d11f
hanniel09 <hannielvieira1227@gmail.com>
2023-05-22 16:59:00 -0300

    Feature

--- maratona-java/src/estudos/maratonajava/javacore/formatacao/test/SimpleDateFormatTest01.java
+++ maratona-java/src/estudos/maratonajava/javacore/formatacao/test/SimpleDateFormatTest01.java
@@ -1,9 +1,19 @@
 package estudos.maratonajava.javacore.formatacao.test;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SimpleDateFormatTest01 {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat();
+        String pattern = "'Amsterdam' dd 'de' MMMM 'de' yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        System.out.println(sdf.format(new Date()));
+        try {
+            System.out.println(sdf.parse("Amsterdam 22 de May de 2023"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
+
 }
