046b4ecd27d1136f9abe5c5b58fabd53998616a3
hanniel09 <hannielvieira1227@gmail.com>
2023-05-23 10:31:47 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/dates/test/InstantTest01.java
@@ -0,0 +1,12 @@
+package estudos.maratonajava.javacore.dates.test;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public class InstantTest01 {
+    public static void main(String[] args) {
+        Instant now = Instant.now();
+        System.out.println(now);
+        System.out.println(LocalDateTime.now());
+    }
+}
