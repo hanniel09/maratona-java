48bcf8d99a1221b18683b80ac6d35f51aedc853b
hanniel09 <hannielvieira1227@gmail.com>
2023-05-24 15:28:43 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/dates/test/TemporalAdjustersTest01.java
@@ -0,0 +1,20 @@
+package estudos.maratonajava.javacore.dates.test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAdjusters;
+
+public class TemporalAdjustersTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        now = now.with(ChronoField.DAY_OF_MONTH, 20);
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.firstDayOfNextMonth());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+    }
+}
