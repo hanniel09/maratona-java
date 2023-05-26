061f3cfc4fecdedb802fcd8931d56243bd30f3bf
hanniel09 <hannielvieira1227@gmail.com>
2023-05-26 10:25:42 -0300

    Feature

--- maratona-java/src/estudos/maratonajava/javacore/regex/test/PatternMatcherTest02.java
+++ maratona-java/src/estudos/maratonajava/javacore/regex/test/PatternMatcherTest02.java
@@ -7,10 +7,13 @@ public class PatternMatcherTest02 {
     public static void main(String[] args) {
         // \d = Todos os dígitos numericos
         // \D = Tudo o que não for digitos
-        // \s = Espaços em branco
-        String regex = "\\D";
+        // \s = Espaços em branco \t \n \f \r
+        // \S = Todos os caracters excluindo os brancos
+        // \w = a-z A-Z, digítos, _
+        // \W = Tudo que não for inclusivo no \w
+        String regex = "\\W";
         String texto = "abaaba";
-        String texto2 = "@#hhj212gvh21kh";
+        String texto2 = "@#hh_j212g vh21kh";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto2);
         System.out.println("texto:  " + texto2);
