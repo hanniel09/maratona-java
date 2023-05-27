9f795504da39d249d6cab90b2a2114ec6b18690f
hanniel09 <hannielvieira1227@gmail.com>
2023-05-27 18:38:43 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/io/test/FileTest02.java
@@ -0,0 +1,22 @@
+package estudos.maratonajava.javacore.io.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileTest02 {
+    public static void main(String[] args) throws IOException {
+        File fileDiretorio = new File("pasta");
+        boolean isDiretorioCreated = fileDiretorio.mkdir();
+        System.out.println(isDiretorioCreated);
+        File fileArquivoDiretorio = new File(fileDiretorio,"arquivo.txt");
+        boolean isFileCreated = fileArquivoDiretorio.createNewFile();
+        System.out.println(isFileCreated);
+        File fileRenamed = new File(fileDiretorio,"arquivo_renomeado.txt");
+        boolean isRenamed = fileArquivoDiretorio.renameTo(fileRenamed);
+        System.out.println(isRenamed);
+
+        File diretorioRenamed = new File("pasta2");
+        boolean isDiretorioRenamed = fileDiretorio.renameTo(diretorioRenamed);
+        System.out.println(isDiretorioRenamed);
+    }
+}
