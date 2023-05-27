b7209cf98dc4e5d7eff20cfafa5da1fb4bee1230
hanniel09 <hannielvieira1227@gmail.com>
2023-05-27 18:03:30 -0300

    Feature

--- maratona-java/src/estudos/maratonajava/javacore/io/test/BufferedWriterTest01.java
+++ maratona-java/src/estudos/maratonajava/javacore/io/test/BufferedWriterTest01.java
@@ -9,10 +9,10 @@ public class BufferedWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try ( FileWriter fw = new FileWriter(file, true);
-              BufferedWriter br = new BufferedWriter(fw)){
-            br.write("Hanniel is the best developer in the world");
-            br.newLine();
-            br.flush();
+              BufferedWriter bw = new BufferedWriter(fw)){
+            bw.write("Hanniel is the best developer in the world");
+            bw.newLine();
+            bw.flush();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
