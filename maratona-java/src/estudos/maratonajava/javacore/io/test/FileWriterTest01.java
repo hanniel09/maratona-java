4ca2e97672415b6bc9bd866f76a53b475a12e17b
hanniel09 <hannielvieira1227@gmail.com>
2023-05-27 16:38:30 -0300

    Feature

--- maratona-java/src/estudos/maratonajava/javacore/io/test/FileWriterTest01.java
+++ maratona-java/src/estudos/maratonajava/javacore/io/test/FileWriterTest01.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class FileWriter01 {
+public class FileWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try ( FileWriter fw = new FileWriter(file, true)){
