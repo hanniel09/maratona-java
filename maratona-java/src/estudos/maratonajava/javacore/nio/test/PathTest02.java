7b678da2738ce8e16e69bbe71761606d9d5d6eb7
hanniel09 <hannielvieira1227@gmail.com>
2023-05-28 16:12:45 -0300

    Feature

--- maratona-java/src/estudos/maratonajava/javacore/nio/test/PathTest02.java
+++ maratona-java/src/estudos/maratonajava/javacore/nio/test/PathTest02.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class PathTest02 {
     public static void main(String[] args) throws IOException {
@@ -13,5 +14,14 @@ public class PathTest02 {
         }
         Path subPastaPath = Paths.get("pasta/subpasta/subsubpasta");
         Path subPastaDirectory = Files.createDirectories(subPastaPath);
+        Path filePath = Paths.get(subPastaPath.toString(), "file.txt");
+
+        if(Files.notExists(pastaPath)) {
+            Path filePathCreated = Files.createFile(filePath);
+        }
+
+        Path source = filePath;
+        Path target = Paths.get(filePath.getParent().toString(), "file_renamed.txt");
+        Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
     }
 }
