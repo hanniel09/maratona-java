db4f0ee5e579df76b9cd28c7db3d7a48d5467df8
hanniel09 <hannielvieira1227@gmail.com>
2023-05-28 15:16:36 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/nio/test/PathTest01.java
@@ -0,0 +1,18 @@
+package estudos.maratonajava.javacore.nio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest01 {
+    public static void main(String[] args) {
+        Path p1 = Paths.get("C:\\Users\\hanni\\Desktop\\Estudos-Java\\maratona-java\\file.txt");
+        Path p2 = Paths.get("C:\\Users\\hanni\\Desktop\\Estudos-Java\\maratona-java", "file.txt");
+        Path p3 = Paths.get("C:", "\\Users\\hanni\\Desktop\\Estudos-Java\\maratona-java", "file.txt");
+        Path p4 = Paths.get("C:", "Users", "hanni", "Desktop", "Estudos-Java", "maratona-java", "file.txt");
+        System.out.println(p1.getFileName());
+        System.out.println(p2.getFileName());
+        System.out.println(p3.getFileName());
+        System.out.println(p4.getFileName());
+
+    }
+}
