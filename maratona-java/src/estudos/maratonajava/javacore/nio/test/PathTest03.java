ed978e53b6b5f6c85ce6f24590276d23c402708f
hanniel09 <hannielvieira1227@gmail.com>
2023-05-29 11:58:35 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/nio/test/PathTest03.java
@@ -0,0 +1,24 @@
+package estudos.maratonajava.javacore.nio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest03 {
+    public static void main(String[] args) {
+        Path dir = Paths.get("home/Hanniel");
+        Path arquivo = Paths.get("dev/arquivo.txt");
+        Path resolve = dir.resolve(arquivo);
+        System.out.println(resolve);
+
+        Path absoluto = Paths.get("/home/Hanniel");
+        Path relativo = Paths.get("dev");
+        Path file = Paths.get("files.txt");
+        System.out.println("1 " + absoluto.resolve(relativo));
+        System.out.println("2 " + absoluto.resolve(file));
+        System.out.println("3 " + relativo.resolve(absoluto));
+        System.out.println("4 " + relativo.resolve(file));
+        System.out.println("5 " + file.resolve(absoluto));
+        System.out.println("6 " + file.resolve(relativo));
+
+    }
+}
