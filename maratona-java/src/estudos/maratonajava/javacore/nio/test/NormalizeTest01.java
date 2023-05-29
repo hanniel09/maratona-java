4f22101057bc240625470693a69c97e72355d06c
hanniel09 <hannielvieira1227@gmail.com>
2023-05-29 10:52:17 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/nio/test/NormalizeTest01.java
@@ -0,0 +1,17 @@
+package estudos.maratonajava.javacore.nio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NormalizeTest01 {
+    public static void main(String[] args) {
+        String diretorioProjeto = "home/Hanniel/Dev";
+        String arquivoTxt = "../../arquivo.txt";
+        Path path1 = Paths.get(diretorioProjeto, arquivoTxt);
+        System.out.println(path1);
+        System.out.println(path1.normalize());
+        Path path2 = Paths.get("/home/./Hanniel/./Dev/");
+        System.out.println(path2);
+        System.out.println(path2.normalize());
+    }
+}
