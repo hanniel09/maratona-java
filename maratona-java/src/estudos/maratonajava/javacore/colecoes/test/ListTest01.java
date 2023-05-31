00c966df122a3f2899b0a5c962ae29ed893fd7e0
hanniel09 <hannielvieira1227@gmail.com>
2023-05-31 16:52:15 -0300

    Feature

--- maratona-java/src/estudos/maratonajava/javacore/colecoes/test/ListTest01.java
+++ maratona-java/src/estudos/maratonajava/javacore/colecoes/test/ListTest01.java
@@ -6,17 +6,28 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>(16);
+        List<String> nomes2 = new ArrayList<>(16);
         nomes.add("Hanniel");
-        nomes.add("Developer Java");
+        nomes.add("Developer");
+        nomes2.add("Vieira");
+        nomes2.add("Java");
 
+        // System.out.println(nomes.remove("developer java"));
+
+        nomes.addAll(nomes2);
         for (String nome : nomes) {
             System.out.println(nome);
         }
-        nomes.add("Vieira");
+
         System.out.println("-----------");
 
         for (int i = 0; i < nomes.size(); i++) {
             System.out.println(nomes.get(i));
         }
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+        System.out.println(numeros);
+
     }
 }
