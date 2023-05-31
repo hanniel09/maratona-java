626dc056f56116682afccbe1caf994b46b241d10
hanniel09 <hannielvieira1227@gmail.com>
2023-05-31 11:52:49 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/colecoes/dominio/Smartophone.java
@@ -0,0 +1,27 @@
+package estudos.maratonajava.javacore.colecoes.dominio;
+
+public class Smartophone {
+    private String serialNumber;
+    private String marca;
+
+    public Smartophone(String serialNumber, String marca) {
+        this.serialNumber = serialNumber;
+        this.marca = marca;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+}
