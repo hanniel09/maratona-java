7bd36e8708cd0f7d68846a6926f6c7335d8d5c98
hanniel09 <hannielvieira1227@gmail.com>
2023-06-01 16:38:46 -0300

    Feature

--- maratona-java/src/estudos/maratonajava/javacore/colecoes/test/NavigableSetTest01.java
+++ maratona-java/src/estudos/maratonajava/javacore/colecoes/test/NavigableSetTest01.java
@@ -15,22 +15,46 @@ class SmartPhoneMarcaComparator implements Comparator<Smartphone> {
         return o1.getMarca().compareTo(o2.getMarca());
     }
 }
+
+class MangaPrecoComparator implements Comparator<Manga>{
+
+    @Override
+    public int compare(Manga o1, Manga o2) {
+        return Double.compare(o1.getPreco(), o2.getPreco());
+    }
+}
 public class NavigableSetTest01 {
     public static void main(String[] args) {
         NavigableSet<Smartphone> set = new TreeSet<>(new SmartPhoneMarcaComparator());
         Smartphone smartphone = new Smartphone("123", "Nokia");
         set.add(smartphone);
 
-        NavigableSet<Manga> mangas = new TreeSet<>();
+        NavigableSet<Manga> mangas = new TreeSet<>(new MangaPrecoComparator());
         mangas.add(new Manga(5L, "Berserk", 19.9, 5));
         mangas.add(new Manga(4L,"Hellsing Ultimate",9.5, 0));
         mangas.add(new Manga(1L,"Pokemon",3.2, 0));
         mangas.add(new Manga(3L,"Attack on titan",11.20, 2));
         mangas.add(new Manga(2L,"Dragon ball Z",2.99, 0));
-        mangas.add(new Manga(10L,"Jujutsu no Kaisen",2.99, 0));
+        mangas.add(new Manga(10L,"Jujutsu no Kaisen",2.95, 0));
+
         for (Manga manga : mangas) {
             System.out.println(manga);
         }
 
+        Manga yuyu = new Manga(6L, "Yuyu Hakusho", 3.2, 5);
+
+        // lower <
+        // floor <=
+        // higher >
+        // ceiling >=
+        System.out.println("----------");
+        System.out.println(mangas.lower(yuyu));
+        System.out.println(mangas.floor(yuyu));
+        System.out.println(mangas.higher(yuyu));
+        System.out.println(mangas.ceiling(yuyu));
+
+        System.out.println(mangas.size());
+        System.out.println(mangas.pollFirst());
+        System.out.println(mangas.size());
     }
 }
