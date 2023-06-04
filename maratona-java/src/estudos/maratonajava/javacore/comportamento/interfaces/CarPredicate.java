93be995acc46e3fc56a38ff8798f6f8b3258f0e9
hanniel09 <hannielvieira1227@gmail.com>
2023-06-03 23:49:55 -0300

    Feature

--- maratona-java/src/estudos/maratonajava/javacore/comportamento/interfaces/CarPredicate.java
+++ maratona-java/src/estudos/maratonajava/javacore/comportamento/interfaces/CarPredicate.java
@@ -3,5 +3,8 @@ package estudos.maratonajava.javacore.comportamento.interfaces;
 import estudos.maratonajava.javacore.comportamento.dominio.Car;
 
 public interface CarPredicate {
+    // anonimas, funções, conciso
     boolean test(Car car);
+    //(parametro) -> <expressão>
+    //(Car car) -> car.getColor().equals("green");
 }
