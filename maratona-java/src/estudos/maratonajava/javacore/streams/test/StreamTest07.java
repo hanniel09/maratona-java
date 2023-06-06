b45747bbeda935eddcc923bf332c4881105dd55e
hanniel09 <hannielvieira1227@gmail.com>
2023-06-06 01:03:42 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/streams/test/StreamTest07.java
@@ -0,0 +1,29 @@
+package estudos.maratonajava.javacore.streams.test;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamTest07 {
+    public static void main(String[] args) {
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6);
+
+        integers.stream().reduce((x, y) -> x + y).ifPresent(System.out::println);
+        System.out.println( integers.stream().reduce(1, (x, y) -> x + y));
+
+        integers.stream().reduce(Integer::sum).ifPresent(System.out::println);
+        System.out.println( integers.stream().reduce(0, Integer::sum));
+
+        System.out.println("----------");
+
+        integers.stream().reduce((x, y) -> x * y).ifPresent(System.out::println);
+        System.out.println( integers.stream().reduce(1,(x, y) -> x * y));
+
+        System.out.println("----------");
+
+        integers.stream().reduce((x, y) -> x > y ? x : y).ifPresent(System.out::println);
+        System.out.println( integers.stream().reduce(0, Integer::max));
+    }
+}
