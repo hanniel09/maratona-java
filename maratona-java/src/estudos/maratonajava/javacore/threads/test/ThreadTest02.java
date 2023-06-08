d05c81d4810b5e445dac99b91a46835af9f37a9a
hanniel09 <hannielvieira1227@gmail.com>
2023-06-08 10:36:05 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/threads/test/ThreadTest02.java
@@ -0,0 +1,36 @@
+package estudos.maratonajava.javacore.threads.test;
+
+
+class ThreadExampleRunnable2 implements Runnable{
+    private final String c;
+
+    public ThreadExampleRunnable2(String c){
+        this.c = c;
+    }
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        for(int i = 0; i < 500; i++){
+            System.out.print(c);
+            if(i % 100 == 0){
+                System.out.println();
+            }
+//            try {
+//                Thread.sleep(0);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+        }
+    }
+}
+public class ThreadTest02 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new ThreadExampleRunnable2("KA"));
+        Thread t2 = new Thread(new ThreadExampleRunnable2("ME"));
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t1.start();
+        t1.join();
+        t2.start();
+
+    }
+}
