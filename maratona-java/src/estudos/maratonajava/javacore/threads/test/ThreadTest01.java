511f7e702aca752f7c618de2d1edc0f04df89a45
hanniel09 <hannielvieira1227@gmail.com>
2023-06-08 00:23:18 -0300

    Feature

--- maratona-java/src/estudos/maratonajava/javacore/threads/test/ThreadTest01.java
+++ maratona-java/src/estudos/maratonajava/javacore/threads/test/ThreadTest01.java
@@ -49,5 +49,6 @@ public class ThreadTest01 {
           t2.start();
           t3.start();
           t4.start();
+        System.out.println("####################### " + Thread.currentThread().getName());
     }
 }
