9b4d1692d26ae06889f9217e3aef58ae30702eea
hanniel09 <hannielvieira1227@gmail.com>
2023-06-08 23:44:02 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/threads/service/EmailDeliveryService.java
@@ -0,0 +1,29 @@
+package estudos.maratonajava.javacore.threads.service;
+
+import estudos.maratonajava.javacore.threads.dominio.Members;
+
+public class EmailDeliveryService implements Runnable {
+    private final Members members;
+
+    public EmailDeliveryService(Members members) {
+        this.members = members;
+    }
+
+    @Override
+    public void run() {
+        String threadName = Thread.currentThread().getName();
+        System.out.println(threadName + " Starting to deliver emails...");
+        while (members.isOpen() || members.pendingEmails() > 0) {
+            try {
+                String email = members.retrieveEmails();
+                if (email == null) continue;
+                System.out.println(threadName + "enviando email para " + email);
+                Thread.sleep(2000);
+                System.out.println(threadName + " enviou email com sucesso para " + email);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Todos os emails foram enviados com sucesso");
+    }
+}
---
+++ maratona-java/src/estudos/maratonajava/javacore/threads/test/EmailDeliveryTest01.java
@@ -0,0 +1,25 @@
+package estudos.maratonajava.javacore.threads.test;
+
+import estudos.maratonajava.javacore.threads.dominio.Members;
+import estudos.maratonajava.javacore.threads.service.EmailDeliveryService;
+
+import javax.swing.*;
+
+public class EmailDeliveryTest01 {
+    public static void main(String[] args) {
+        Members members = new Members();
+        Thread jiraya = new Thread(new EmailDeliveryService(members), "Jiraya");
+        Thread kakashi = new Thread(new EmailDeliveryService(members), "Kakashi");
+        jiraya.start();
+        kakashi.start();
+        while (true){
+            String email = JOptionPane.showInputDialog("Entre com seu email");
+            if (email == null || email.isEmpty()){
+                members.close();
+                break;
+            }
+            members.addMemberEmail(email);
+        }
+
+    }
+}
