9ae5273289f468076e78d40b553e818e53bd60e7
hanniel09 <hannielvieira1227@gmail.com>
2023-06-09 18:02:04 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/concorrencia/test/ReentrantReadWriteLockTest01.java
@@ -0,0 +1,57 @@
+package estudos.maratonajava.javacore.concorrencia.test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+class MapReadWrite {
+    private final Map<String, String> map = new LinkedHashMap<>();
+    private final ReentrantReadWriteLock rwl;
+
+    public MapReadWrite(ReentrantReadWriteLock rwl) {
+        this.rwl = rwl;
+    }
+
+    public void put(String key, String value) {
+        rwl.writeLock().lock();
+        try {
+            if (rwl.isWriteLocked()) {
+                System.out.printf("%s obteve o Write lock", Thread.currentThread().getName());
+            }
+            map.put(key, value);
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            rwl.writeLock().unlock();
+        }
+    }
+
+    public Set<String> allKeys(){
+        rwl.readLock().lock();
+        try{
+            return map.keySet();
+        }finally {
+            rwl.readLock().unlock();
+        }
+    }
+}
+
+public class ReentrantReadWriteLockTest01 {
+    public static void main(String[] args) {
+        ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+        MapReadWrite mapReadWrite = new MapReadWrite(rwl);
+
+        Runnable writer = () -> {
+            for (int i = 0; i < 20; i++) {
+                mapReadWrite.put(String.valueOf(i), String.valueOf(i));
+            }
+        };
+
+        Runnable reader = () -> {
+            if (rwl.isWriteLocked()) {
+
+            }
+        };
+}
