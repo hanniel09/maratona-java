98b9f2bde103765d5443d425699ede93b775a3c6
hanniel09 <hannielvieira1227@gmail.com>
2023-06-11 14:53:07 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/concorrencia/test/CompletableFutureTest02.java
@@ -0,0 +1,29 @@
+package estudos.maratonajava.javacore.concorrencia.test;
+
+import estudos.maratonajava.javacore.concorrencia.service.StoreService;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CompletableFutureTest02 {
+    public static void main(String[] args) {
+        StoreService storeService = new StoreService();
+        searchPricesAsyncCompletableFuture(storeService);
+    }
+
+    private static void searchPricesAsyncCompletableFuture(StoreService storeService) {
+        long start = System.currentTimeMillis();
+        List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
+
+        List<CompletableFuture<Double>> completableFutures = stores.stream()
+                .map(storeService::getPricesAsyncCompletableFuture)
+                .collect(Collectors.toList());
+        List<Double> prices = completableFutures.stream().map(CompletableFuture::join).collect(Collectors.toList());
+
+        System.out.println(prices);
+        long end = System.currentTimeMillis();
+        System.out.printf("Time passed to searchPricesSync %dms%n", (end - start));
+    }
+}
