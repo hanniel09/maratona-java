c41e65c44b863409b1222864c16adac8b074a0d3
hanniel09 <hannielvieira1227@gmail.com>
2023-06-11 00:02:18 -0300

    Feature

--- maratona-java/src/estudos/maratonajava/javacore/concorrencia/test/CallableTest01.java
+++ maratona-java/src/estudos/maratonajava/javacore/concorrencia/test/CallableTest01.java
@@ -8,18 +8,18 @@ class RandomNumberCallable implements Callable<String>{
     public String call() throws Exception {
         int number = ThreadLocalRandom.current().nextInt(1, 11);
         for (int i = 0; i < number; i++) {
-            System.out.printf("%s executando uma tarefa Callable", Thread.currentThread().getName());
+            System.out.printf("%s executando uma tarefa Callable...%n", Thread.currentThread().getName());
         }
         return String.format("%s finished and the random number is %d", Thread.currentThread().getName(), number);
     }
 }
 public class CallableTest01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         RandomNumberCallable randomNumberCallable = new RandomNumberCallable();
         ExecutorService executorsService = Executors.newFixedThreadPool(2);
         Future<String> future = executorsService.submit(randomNumberCallable);
         String s = future.get();
-        System.out.printf("Program finished %s", ss);
+        System.out.printf("Program finished %s%n", s);
         System.out.println("Program finished");
         executorsService.shutdown();
     }
