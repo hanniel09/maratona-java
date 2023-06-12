91519f0ccaf5a327879e499833993543371c95d9
hanniel09 <hannielvieira1227@gmail.com>
2023-06-12 15:53:57 -0300

    Feature

---
+++ maratona-java/src/estudos/maratonajava/javacore/padroesdeprojeto/test/BuilderPatternTest01.java
@@ -0,0 +1,16 @@
+package estudos.maratonajava.javacore.padroesdeprojeto.test;
+
+import estudos.maratonajava.javacore.padroesdeprojeto.dominio.Person;
+
+public class BuilderPatternTest01 {
+    public static void main(String[] args) {
+        Person build = new Person.PersonBuilder()
+                .firstName("Hanniel")
+                .lastName("Vieira")
+                .userName("Backend Develepor")
+                .email("Hanniel@QueroUmEmprego.com")
+                .build();
+        System.out.println(build);
+
+    }
+}
